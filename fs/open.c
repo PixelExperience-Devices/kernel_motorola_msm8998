2eca7081ff911474425f7e05d0d709e3c7f75aac
Yaroslav Furman <yaro330@gmail.com>
2020-01-11 22:01:01 +0300

    fs: add a simplified file blocker
    fs/exec.c part was not needed.
    
    [from prev version]:
    Unlike previous versions of this monstrocity, this version
    doesn't hide anything, it just makes files un-executable.
    
    file_blocker.h contains two arrays, paths and files.
    If a path from blocked list is detected check_file() function
    blocks access to files that contain string from blocked files list
    in their path.
    
    Inspired by:
    the PUBG blocker [1] from Diab Neiroukh <officiallazerl0rd@gmail.com>
    And commit [2] from Demon000 <demonsingur@gmail.com>
    https://github.com/RaphielGang/spins_kernel_xiaomi_sdm845/commit/75804821e68be3ece
    https://github.com/Demon000/kernel_xiaomi_sm8150/commit/cf076061980b95ce
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: itsjoeoui <maximeyxw@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -903,7 +903,7 @@ struct file *dentry_open(const struct path *path, int flags,
 				fput(f);
 				f = ERR_PTR(error);
 			}
-		} else { 
+		} else {
 			put_filp(f);
 			f = ERR_PTR(error);
 		}
@@ -1019,7 +1019,7 @@ struct file *filp_open(const char *filename, int flags, umode_t mode)
 {
 	struct filename *name = getname_kernel(filename);
 	struct file *file = ERR_CAST(name);
-	
+
 	if (!IS_ERR(name)) {
 		file = file_open_name(name, flags, mode);
 		putname(name);
@@ -1039,6 +1039,61 @@ struct file *file_open_root(struct dentry *dentry, struct vfsmount *mnt,
 }
 EXPORT_SYMBOL(file_open_root);
 
+struct file *filp_clone_open(struct file *oldfile)
+{
+	struct file *file;
+	int retval;
+
+	file = get_empty_filp();
+	if (IS_ERR(file))
+		return file;
+
+	file->f_flags = oldfile->f_flags;
+	retval = vfs_open(&oldfile->f_path, file, oldfile->f_cred);
+	if (retval) {
+		put_filp(file);
+		return ERR_PTR(retval);
+	}
+
+	return file;
+}
+EXPORT_SYMBOL(filp_clone_open);
+
+#ifdef CONFIG_BLOCK_UNWANTED_FILES
+static char *files_array[] = {
+	"fde",
+	"lspeed",
+	"nfsinjector",
+	"lkt",
+};
+
+static char *paths_array[] = {
+	"/data/adb/modules",
+};
+
+static bool inline check_file(const char *name)
+{
+	int i, f;
+	for (f = 0; f < ARRAY_SIZE(paths_array); ++f) {
+		const char *path_to_check = paths_array[f];
+
+		if (!strncmp(name, path_to_check, strlen(path_to_check))) {
+			for (i = 0; i < ARRAY_SIZE(files_array); ++i) {
+				const char *filename = name + strlen(path_to_check) + 1;
+				const char *filename_to_check = files_array[i];
+
+				/* Leave only the actual filename for strstr check */
+				if (strstr(filename, filename_to_check)) {
+					pr_info("%s: blocking %s\n", __func__, filename);
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
+}
+#endif
+
 long do_sys_open(int dfd, const char __user *filename, int flags, umode_t mode)
 {
 	struct open_flags op;
@@ -1051,6 +1106,13 @@ long do_sys_open(int dfd, const char __user *filename, int flags, umode_t mode)
 	tmp = getname(filename);
 	if (IS_ERR(tmp))
 		return PTR_ERR(tmp);
+
+#ifdef CONFIG_BLOCK_UNWANTED_FILES
+	if (unlikely(check_file(tmp->name))) {
+		putname(tmp);
+		return fd;
+	}
+#endif
 
 	fd = get_unused_fd_flags(flags);
 	if (fd >= 0) {
