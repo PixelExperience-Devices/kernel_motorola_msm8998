49f32e99fb52297f8af652b17a779b27985171d1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-28 23:10:29 +0200

    init: Add CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
    * Ignoring an ignore flag, yikes
    * Also replace skip_initramf with want_initramf (omitting last letter for Magisk since it binary patches that out of kernel, I'm not even sure why we're supporting that mess)
    
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Change-Id: Ifdf726f128bc66bf860bbb71024f94f56879710f

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,13 +2,37 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #include <asm/setup.h>
+#endif
 
-static char new_command_line[COMMAND_LINE_SIZE];
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+#define INITRAMFS_STR_FIND "skip_initramf"
+#define INITRAMFS_STR_REPLACE "want_initramf"
+#define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
+
+static char proc_command_line[COMMAND_LINE_SIZE];
+
+static void proc_command_line_init(void) {
+	char *offset_addr;
+
+	strcpy(proc_command_line, saved_command_line);
+
+	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
+	if (!offset_addr)
+		return;
+
+	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
+}
+#endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", new_command_line);
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+	seq_printf(m, "%s\n", proc_command_line);
+#else
+	seq_printf(m, "%s\n", saved_command_line);
+#endif
 	return 0;
 }
 
@@ -26,26 +50,9 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
-
-	char *search = "skip_initramf";
-	char *replace = "androidboot.force_normal_boot=1 ";
-
-	char *offset_addr, *cmd = new_command_line;
-	size_t search_len, replace_len;
-
-	strcpy(cmd, saved_command_line);
-
-        search_len = strlen(search);
-        replace_len = strlen(replace);
-
-	offset_addr = strstr(cmd, search);
-	if (offset_addr) {
-		size_t tail_len;
-		tail_len = strlen(offset_addr+search_len);
-
-		memmove(offset_addr+replace_len,offset_addr+search_len,tail_len+1);
-		memcpy(offset_addr, replace, replace_len);
-	}
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+	proc_command_line_init();
+#endif
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
